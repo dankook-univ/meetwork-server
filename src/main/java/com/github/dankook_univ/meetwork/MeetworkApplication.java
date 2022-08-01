ca63a4141ff15ef2d152e14d6fb8ecc248b22985
Bae-jihyun <govl6113@gmail.com>
2022-08-02 00:33:23 +0900

    feat: project setup

---
+++ src/main/java/com/github/dankook_univ/meetwork/MeetworkApplication.java
@@ -0,0 +1,13 @@
+package com.github.dankook_univ.meetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/dankook_univ/meetwork/MeetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dankook_univ.meetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
