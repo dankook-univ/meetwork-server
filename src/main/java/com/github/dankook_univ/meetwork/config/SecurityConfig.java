0178b2cdd6a1fdd7870823339ea0d70d269dbcb5
himitery <himit0131@gmail.com>
2022-08-07 08:54:13 +0900

    fix: cors 설정 및 core entity 수정

--- src/main/java/com/github/dankook_univ/meetwork/config/SecurityConfig.java
+++ src/main/java/com/github/dankook_univ/meetwork/config/SecurityConfig.java
@@ -9,37 +9,57 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final TokenProviderImpl tokenProvider;
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+	private final TokenProviderImpl tokenProvider;
+	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.csrf().disable()
+                
+				.cors().configurationSource(corsConfigurationSource())
+				.and()
 
-            .apply(new JwtSecurityConfig(tokenProvider))
-            .and()
+				.apply(new JwtSecurityConfig(tokenProvider))
+				.and()
 
-            .exceptionHandling()
-            .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-            .accessDeniedHandler(jwtAccessDeniedHandler)
-            .and()
+				.exceptionHandling()
+				.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+				.accessDeniedHandler(jwtAccessDeniedHandler)
+				.and()
 
-            .authorizeRequests()
-            .antMatchers("/").permitAll()
-            .antMatchers("/swagger-ui/**").permitAll()
-            .antMatchers("/swagger-resources/**").permitAll()
-            .antMatchers("/v3/api-docs").permitAll()
-            .antMatchers("/api/auth/**").permitAll()
-            .antMatchers("/ws/**").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .build();
-    }
+				.authorizeRequests()
+				.antMatchers("/").permitAll()
+				.antMatchers("/swagger-ui/**").permitAll()
+				.antMatchers("/swagger-resources/**").permitAll()
+				.antMatchers("/v3/api-docs").permitAll()
+				.antMatchers("/api/auth/**").permitAll()
+				.antMatchers("/ws/**").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.build();
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.addAllowedOrigin("http://localhost:3000");
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedMethod("*");
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
 }
