b5e5faa01737208e294f454dfc9e044e20bb29f5
Bae-jihyun <govl6113@gmail.com>
2022-08-07 16:45:23 +0900

    refactor(board): 주석 제거

--- src/main/java/com/github/dankook_univ/meetwork/member/infra/persistence/MemberJpaRepository.java
+++ src/main/java/com/github/dankook_univ/meetwork/member/infra/persistence/MemberJpaRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberJpaRepository extends JpaRepository<Member, UUID> {
 
-//    Optional<Member> findById(UUID id);
 }
